80805ff3a629d097b747d4ab3b6423c2092faada
raviteja9135 <35614976+raviteja9135@users.noreply.github.com>
2024-05-10 22:59:40 +0530

    adding common module

--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -2,13 +2,15 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { DashboardComponent } from './dashboard/dashboard.component';
 import { HomeComponent } from './home/home.component';
+import { CommonUtilsModule } from '../common-utils/common-utils.module';
 
 
 
 @NgModule({
   declarations: [DashboardComponent,HomeComponent],
   imports: [
-    CommonModule
+    CommonModule,
+    CommonUtilsModule
   ]
 })
 export class HomeModule { }
